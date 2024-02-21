d1ddf259b9b5c4e0f60175e18173fed6535ed660
punchisv <punchisv@yahoo.com.au>
2024-02-21 15:15:58 +0930

    Corrected the code 'validatons.py'

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -13,21 +13,18 @@ def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
-    if username[0].isalpha():  #not re.match('^[a-z0-9._]*$', username):
-        print(username)
-        if username[0] == "_": 
-            return False
-        else:
-            return True
-    # Usernames can't begin with a number
+    if not re.search(r'^[a-z0-9]', username):
+        #print(username)
+        return False
+        # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    else:
-        return True
+    
+    return True
 
 print(validate_user("blue.kale", 3))# True
 print(validate_user(".blue.kale", 3))# False
 print(validate_user("red_quinoa", 4))# True
-print(validate_user("_red_quinoa", 4))# True
+print(validate_user("_red_quinoa", 4))# False
 
 
